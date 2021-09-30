7d280952b72855b48abf2fce54993b8eb63b9955
Nestor Monroy <nestor.monroy.90@gmail.com>
2021-09-30 15:47:09 -0500

    [add] example add a student to the database

---
+++ JavaAplicaciones/jdbc/student/add-student/src/main/java/com.nestor.addstudent/StudentController.java
@@ -0,0 +1,71 @@
+package com.nestor.addstudent;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@SessionScoped
+public class StudentController {
+
+    private List<Student> students;
+    private final StudentDbUtil studentDbUtil;
+    private final Logger logger = Logger.getLogger(getClass().getName());
+
+    public StudentController() throws Exception {
+        students = new ArrayList<>();
+
+        studentDbUtil = StudentDbUtil.getInstance();
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void loadStudents() {
+
+        logger.info("Loading students");
+
+        students.clear();
+
+        try {
+
+            // get all students from database
+            students = studentDbUtil.getStudents();
+
+        } catch (Exception exc) {
+            // send this to server logs
+            logger.log(Level.SEVERE, "Error loading students", exc);
+
+            // add error message for JSF page
+            addErrorMessage(exc);
+        }
+    }
+
+    public String addStudent(Student theStudent) {
+        logger.info("Adding student: " + theStudent);
+        try {
+            // add student to the database
+            studentDbUtil.addStudent(theStudent);
+        } catch (Exception exc) {
+            // send this to server logs
+            logger.log(Level.SEVERE, "Error adding students", exc);
+            // add error message for JSF page
+            addErrorMessage(exc);
+            return null;
+        }
+
+        return "list-students?faces-redirect=true";
+    }
+
+    private void addErrorMessage(Exception exc) {
+        FacesMessage message = new FacesMessage("Error: " + exc.getMessage());
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+
+}
